2c268a7c660b6dac9f13074dfcaedb904d2f0fb8
weijian <weijian.yang@thoughtworks.com>
2020-03-01 16:28:39 +0800

    update junit version to 5
    update junit version to 5
    
    update junit version to 5

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,9 +1,8 @@
 package com.gildedrose;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GildedRoseTest {
 
@@ -12,9 +11,8 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 1, 5) };
         GildedRose app = new GildedRose(items);
         app.update_quality();
-        assertEquals("foo", app.items[0].name);
-        assertThat(app.items[0].quality, is(4));
-        assertThat(app.items[0].sell_in, is(0));
+        assertThat("foo").isEqualTo(app.items[0].name);
+        assertThat(app.items[0].quality).isEqualTo(4);
+        assertThat(app.items[0].sell_in).isEqualTo(0);
     }
-
 }
