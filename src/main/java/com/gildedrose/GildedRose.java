2ec9b26307c81b371dc5461d13acbd159a94c5b8
weijian <weijian.yang@thoughtworks.com>
2020-03-01 17:22:44 +0800

    use switch replace if...else...

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,48 +9,52 @@ class GildedRose {
 
     public void update_quality() {
         for (Item item : items) {
-            if (item.name.equals("Aged Brie")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                }
-
-                item.sell_in = item.sell_in - 1;
-
-                if (item.sell_in < 0 && item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-            else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.sell_in < 11 && item.quality < 50) {
+            switch (item.name) {
+                case "Aged Brie":
+                    if (item.quality < 50) {
                         item.quality = item.quality + 1;
+
                     }
 
-                    if (item.sell_in < 6 && item.quality < 50) {
+                    item.sell_in = item.sell_in - 1;
+
+                    if (item.sell_in < 0 && item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
-                }
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
 
-                item.sell_in = item.sell_in - 1;
+                        if (item.sell_in < 11 && item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
 
-                if (item.sell_in < 0) {
-                    item.quality = 0;
-                }
-            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        if (item.sell_in < 6 && item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
 
-            } else {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+                    item.sell_in = item.sell_in - 1;
 
-                item.sell_in = item.sell_in - 1;
+                    if (item.sell_in < 0) {
+                        item.quality = 0;
+                    }
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
 
-                if (item.sell_in < 0 && item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+                    break;
+                default:
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+
+                    item.sell_in = item.sell_in - 1;
+
+                    if (item.sell_in < 0 && item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                    break;
             }
         }
     }
