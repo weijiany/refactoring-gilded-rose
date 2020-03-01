380e69071e611124ec3501a1fe3b95788272195c
weijian <weijian.yang@thoughtworks.com>
2020-03-01 17:21:59 +0800

    Float up if with "Backstage passes to a TAFKAL80ETC concert" and "Sulfuras, Hand of Ragnaros"

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -17,45 +17,20 @@ class GildedRose {
 
                 item.sell_in = item.sell_in - 1;
 
-                if (item.sell_in < 0) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                if (item.sell_in < 0 && item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
             }
-            else if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.sell_in = item.sell_in - 1;
-                }
-
-                if (item.sell_in < 0) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                }
-            }
-            else {
+            else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
-                    if (item.sell_in < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                    if (item.sell_in < 11 && item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
 
-                    if (item.sell_in < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                    if (item.sell_in < 6 && item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
 
@@ -63,6 +38,18 @@ class GildedRose {
 
                 if (item.sell_in < 0) {
                     item.quality = 0;
+                }
+            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+
+            } else {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+
+                item.sell_in = item.sell_in - 1;
+
+                if (item.sell_in < 0 && item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             }
         }
